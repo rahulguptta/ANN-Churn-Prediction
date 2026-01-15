c6529274b2719563603f0016ee092f5385e630ad
Rahul Gupta <58649197+rahulguptta@users.noreply.github.com>
2026-01-15 11:00:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import openai
 from langchain_openai import ChatOpenAI
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import ChatPromptTemplate
